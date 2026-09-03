5a357e1e69746f152b5f673599aaf185cabbd07f
N0VAT0R01 <98565053+N0VAT0R01@users.noreply.github.com>
2026-09-03 10:01:54 +1000

    Add files via upload

---
+++ songs.js
@@ -0,0 +1,36 @@
+/*
+  ЗДЕСЬ МОЖНО ДОБАВЛЯТЬ И РЕДАКТИРОВАТЬ ТЕКСТЫ.
+  Не загружайте сюда тексты, права на которые принадлежат третьим лицам,
+  если у вас нет разрешения на их публикацию.
+
+  Формат:
+  { title: 'Название', artist: 'Исполнитель', lyrics: 'Ваш текст...' }
+
+  В lyrics можно использовать обычный перенос строки. Для припева
+  предусмотрен отдельный необязательный массив chorus.
+*/
+
+const songs = [
+  { title: 'Хоп-хей, лалалей', artist: 'Леонид Агутин', lyrics: '', chorus: '' },
+  { title: 'Катюша', artist: 'Песня военных лет', lyrics: '', chorus: '' },
+  { title: 'Седая ночь', artist: 'Юрий Шатунов', lyrics: '', chorus: '' },
+  { title: 'Знаешь ли ты', artist: 'МакSим', lyrics: '', chorus: '' },
+  { title: 'Трава у дома', artist: 'Земляне', lyrics: '', chorus: '' },
+  { title: 'Комсомольск-на-Амуре', artist: 'Штурм', lyrics: '', chorus: '' },
+  { title: 'Осень', artist: 'ДДТ', lyrics: '', chorus: '' },
+  { title: 'Зеленоглазое такси', artist: 'Михаил Боярский', lyrics: '', chorus: '' },
+  { title: '3 сентября', artist: 'Михаил Шуфутинский', lyrics: '', chorus: '' },
+  { title: 'Районы-кварталы', artist: 'Звери', lyrics: '', chorus: '' },
+  { title: 'Белый песок', artist: 'Жанна Фриске', lyrics: '', chorus: '' },
+  { title: 'О боже, какой мужчина!', artist: 'Натали', lyrics: '', chorus: '' },
+  { title: 'Течёт ручей', artist: 'Надежда Кадышева', lyrics: '', chorus: '' },
+  { title: 'Кукла колдуна', artist: 'Король и Шут', lyrics: '', chorus: '' },
+  { title: 'Владивосток 2000', artist: 'Мумий Тролль', lyrics: '', chorus: '' },
+  { title: 'Сансара', artist: 'Баста', lyrics: '', chorus: '' },
+  { title: 'В роще пел соловушка о любви', artist: 'Марина Девятова', lyrics: '', chorus: '' },
+  { title: 'На позиции девушка провожала бойца', artist: 'Песня военных лет', lyrics: '', chorus: '' },
+  { title: 'Крошка моя', artist: 'Руки Вверх!', lyrics: '', chorus: '' },
+  { title: 'Солнышко в руках', artist: 'Исполнитель не указан', lyrics: '', chorus: '' },
+  { title: 'А не спеть ли нам песню о любви', artist: 'Чиж & Co', lyrics: '', chorus: '' },
+  { title: 'Конь', artist: 'Любэ', lyrics: '', chorus: '' }
+];
